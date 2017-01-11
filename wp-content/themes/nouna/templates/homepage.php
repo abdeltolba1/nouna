5bea5fd3edbdc0a323bea6c30ec39acc1d360117
AbdelMounim TOLBA <abdelmounim.tolba@gmail.com>
2017-01-11 21:38:07 +0000

    Setup homepage

---
+++ wp-content/themes/nouna/templates/homepage.php
@@ -0,0 +1,43 @@
+<?php  
+/**
+ * Template Name: Home Page
+ */
+
+// A static comming soon page
+?>
+<!DOCTYPE html> 
+<html lang="fr" xml:lang="fr">
+<head>
+<meta property="og:title" content="nouna.xyz - Coming soon!"/>
+<meta property="og:type" content="Blog"/>
+<meta property="og:url" content="http://nouna.xyz"/>
+<meta property="og:image" content="http://nouna.xyz/og-image.jpg"/>
+<meta property="og:site_name" content="nouna.xyz"/>
+<meta property="og:description" content="Welcome to nouna.xyz!"/>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+<meta name="description" content="Welcome to nouna.xyz!">
+<title>nouna.xyz - Coming soon!</title>
+<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/img/favicon.png" />
+<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/img/favicon.ico"/>
+<link rel="stylesheet" type="text/css" media="all" href="<?php echo get_template_directory_uri(); ?>/temp/reset.css" />
+<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/temp/global.css" />
+</head>
+<!--[if IE 9]><body class="ie ie9"><![endif]-->
+<!--[if IE 8]><body class="ie ie8"><![endif]-->
+<!--[if IE 7]><body class="ie ie7"><![endif]-->
+<!--[if IE 6]><body class="ie ie6"><![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--><body><!--<![endif]-->
+	<div class="global">
+		<div class="main">
+			<div class="content">
+				<img class="visuel" src="img/nouna.png" alt="" />
+				<h1 class="titre">Hello World! <strong>Welcome to nouna.xyz</strong></h1>
+				<p>Soon in this website you’ll get to know me and I’ll get to share with you my work, ideas and learnings in the coolest way I know.</p>
+				<p class="extra">Stay tuned!</p>
+			</div>
+			<span class="centrage"></span>
+		</div>
+	</div>
+</body>
+</html>
